17045353518e34736064484ea2b0f94d076659d3
API <a.pinsker@era.aero>
2025-10-23 10:24:36 +0200

    NEWFEAT: Pridan soubor definujici typy zprav.

---
+++ vr_core/network/comm_contracts.py
@@ -0,0 +1,24 @@
+"""Communication contracts RPI <-> Unity."""
+
+from enum import IntEnum
+
+# pylint: disable=invalid-name
+class MessageType(IntEnum):
+    """Message types for communication between RPI and Unity.
+
+    camelCase used for consistency with Unity definition.
+    """
+
+    imuSensor = 0
+    imuFilter = 1
+    gazeData = 2
+    gazeCalcControl = 3
+    gazeSceneControl = 4
+    trackerControl = 5
+    tcpConfig = 6
+    espConfig = 7
+    tcpLogg = 8
+    espLogg = 9
+    trackerPreview = 10
+    eyePreview = 11
+    eyeImage = 12
